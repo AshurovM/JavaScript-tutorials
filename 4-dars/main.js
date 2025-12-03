a4a5d9750af06795d64d45bad7774c1d8b67954c
Muhammadali <config880@gmail.com>
2025-12-03 18:20:07 +0500

    Difference between If-Else and Switch

---
+++ 4-dars/main.js
@@ -0,0 +1,8 @@
+// switch va if ning farqi:
+// if (shart beriladi)
+// switch (variable yoziladi, 3ta ===ga to'g'ri kelishi shart, '<' '>' operatorlari ishlamaydi)
+
+// if (condition === "yomg'ir")
+// switch (variable) {
+//     case "yomg'ir": console.log("yomg'ir yog'adi");break;
+// }
