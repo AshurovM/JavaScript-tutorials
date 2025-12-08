d84750f9134716122da600e4b36e8b410bd8263e
Muhammadali <config880@gmail.com>
2025-12-08 17:21:54 +0500

    Explain types of Numbers

---
+++ 8-dars/main.js
@@ -0,0 +1,24 @@
+// console.log(100_000_000); // console.log(1000000000) yozilgani bilan bir xil
+
+// console.log(1e6); // 1000000 yozib beradi edan keyin nechta nol qo'yilishi kerakligi yoziladi
+
+// console.log((0.1 + 0.2).toFixed(2)) // toFixed() qavs ichidagi songacha yaxlitaydi va string ko'rinishga o'tkazadi
+
+// let num = '0.1 + 0.2';
+
+// console.log(parseInt(num)); // parseInt() sonni butun qismini qaytaradi
+
+// console.log(parseFloat(num)); // parseFloat() birinchi sonni oladi
+
+// console.log(eval(num)); // Eval ichidagi malumotni numberga o'zgartirib hisoblar beradi
+
+// Math kutubxonasi
+// Math.round(2.4) = 2; Math.round(2.5) = 3 // sonni yaxlitlab beradi
+// Math.floor(1.999) = 1 // sonni eng kichik butun qismigacha yaxlitlaydi
+// Math.ceil(1.001) = 2 // sonni eng katta butun qismigacha yaxlitlaydi
+// Math.trunc(1.999) = 1 // sonni butun qismini olib beradi
+// Math.trunc(Math.random() * 10) // 0 va 1 oralig'idagi ixtiyoriy raqamni qaytaradi
+// Math.min(1, 43, 16, 25, 35, 47, 89, 58) // sonlar orasidagi eng kichigini olib beradi
+// Math.max(1, 43, 16, 25, 35, 47, 89, 58) // sonlar orasidagi eng kattasini olib beradi
+// Math.pow(5, 2); // sonni darajaga oshirib beradi
+// Math.sqrt(9); // sonni ildizini hisoblab beradi
