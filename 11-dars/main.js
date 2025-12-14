e50a6016bd6b3c4aab41042274847eef8b2c22df
Muhammadali <config880@gmail.com>
2025-12-14 18:15:18 +0500

    Introduce Destructuring and Spread operators

---
+++ 11-dars/main.js
@@ -0,0 +1,39 @@
+// Spread
+
+// Spread - obyektlarda va arraylarda ishlaydi
+// const vegetables = ['carrot', 'potato', 'onion']
+// const fruits = ['apple', 'peach', 'orange']
+// console.log([...fruits, ...vegetables]);
+
+// const allFood = vegetables.concat(fruits) // oldingi usuli
+// console.log(allFood);
+
+// Obyektlarda:
+// const harvestOfVegetables = {
+//     carrot: 30,
+//     potato: 40,
+//     onion: 20
+// }
+
+// const harvestOfFruits = {
+//     apple: 15,
+//     peach: 5,
+//     orange: 10
+// }
+// console.log({...harvestOfFruits, ...harvestOfVegetables});
+
+// -------------------------------- Destructuring -------------------------------------------------------
+
+// const [olma, gilos, ...qolganlari] = ['olma', 'gilos', 'uzum', 'nok']
+// console.log(olma, gilos, qolganlari);
+
+// const { name, age, job } = {
+//     name: "John",
+//     age: 21,
+//     job: 'developer'
+// };
+
+// console.log(name, age, job);
+
+
+// const colours = ['red', 'green', 'yellow', 'blue'}
